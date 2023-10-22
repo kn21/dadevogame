5c82cad4399665320d7d3837adf08540570efaf6
dakienn <87208393+kn21@users.noreply.github.com>
2023-10-22 12:14:54 -0700

    add text to script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+//counter
+var counter = document.createTextNode("1"):
 //Devo sprite
 var devo = document.createElement('img');
 devo.src = 'devo.png';
 devo.width = 200;
 devo.height = 200;
 document.body.appendChild(devo);
+document.body.appendChild(counter):
