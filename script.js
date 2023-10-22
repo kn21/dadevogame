44a31e419adc9e29688ad5449176e870e1bd84de
dakienn <87208393+kn21@users.noreply.github.com>
2023-10-22 12:15:32 -0700

    devo size script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var counter = document.createTextNode("1"):
 //Devo sprite
 var devo = document.createElement('img');
 devo.src = 'devo.png';
-devo.width = 200;
-devo.height = 200;
+devo.width = 50;
+devo.height = 50;
 document.body.appendChild(devo);
 document.body.appendChild(counter):
